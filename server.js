f7165e9ba02186d9eb75bf6a2b3227ad5dc6625d
Arezoo Tajerzadeh <Arezoo Tajerzadeh>
2017-04-29 19:09:08 -0700

    clean up

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //install nmp express , body-parser , path
 var express = require("express");
 var bodyParser = require("body-parser");
+
+var app =express();
 var PORT = process.env.PORT || 8080;
 
 
