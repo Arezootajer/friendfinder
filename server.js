7bc76845876ba6164a9b342d60d3b5725527f268
Arezoo Tajerzadeh <Arezoo Tajerzadeh>
2017-04-30 18:16:29 -0700

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //install nmp express , body-parser , path
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app =express();
 var PORT = process.env.PORT || 8080;
